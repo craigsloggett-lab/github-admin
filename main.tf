5c0517d1170715fa41464ffeaa0366d84ddfdb00
Craig Sloggett <craigsloggett@gmail.com>
2025-12-02 11:01:29 -0500

    Add GitHub provider and example output

---
+++ main.tf
@@ -0,0 +1,4 @@
+data "github_repository" "github_admin" {
+  owner = "craigsloggett-lab"
+  name  = "github-admin"
+}
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "github_admin" {
+  value       = data.github_repository.github_admin
+  description = "The result of the GitHub repository data source."
+}
