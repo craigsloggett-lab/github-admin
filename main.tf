12487803f34dd5c389c04cb7a7ab9e0700347268
Craig Sloggett <craigsloggett@gmail.com>
2026-03-28 12:58:48 -0400

    Update repository configuration

--- main.tf
+++ main.tf
@@ -2,4 +2,12 @@ resource "github_repository" "hashistack_aws_vpc" {
   name = "hashistack-aws-vpc"
 
   template_repository = "terraform-root-module-template"
+
+  has_wiki     = false
+  has_projects = false
+
+  allow_merge_commit = false
+  allow_rebase_merge = false
+
+  delete_branch_on_merge = true
 }
