02b49da9510da85ecd55bd442c6196121693d9b5
Craig Sloggett <craigsloggett@gmail.com>
2026-03-30 10:17:11 -0400

    Add pingfederate-artifacts repository (#21)
    * Add pingfederate-artifacts repository
    
    * Regenerate README

--- main.tf
+++ main.tf
@@ -16,3 +16,22 @@ resource "github_repository" "hashistack_aws_vpc" {
 
   topics = ["terraform"]
 }
+
+resource "github_repository" "pingfederate_artifacts" {
+  name        = "pingfederate-artifacts"
+  description = "An infrastructure as code repository to manage the S3 bucket for PingFederate distribution artifacts."
+  homepage    = "https://app.terraform.io/app/craigsloggett-lab/workspaces/pingfederate-artifacts"
+
+  template_repository = "terraform-root-module-template"
+
+  has_wiki     = false
+  has_projects = false
+
+  allow_merge_commit = false
+  allow_rebase_merge = false
+
+  delete_branch_on_merge = true
+  allow_update_branch    = true
+
+  topics = ["terraform"]
+}
